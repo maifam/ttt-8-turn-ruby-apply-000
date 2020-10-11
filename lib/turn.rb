8ab51a8dcca3aa35c4b69edc16a3eb9574d137ab
Mai Pham <maitphm@gmail.com>
2020-10-11 16:01:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,17 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   
-  input = gets.strip 
-  
+  user_input = gets.strip 
+  input_to_index(user_input)
+  if valid_move?(board, index) == true 
+    move(board, index, token = "X")
+    return display_board(board)
+  else 
+    until valid_move?(board, index) == true 
+      puts "Please enter 1-9:"
+      user_input = gets.strip
+      input_to_index(user_input)
+    end 
+  end 
 end 
 
